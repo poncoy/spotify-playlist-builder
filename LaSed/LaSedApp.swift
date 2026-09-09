ed47058e6d5e7f8489e88619c3467c5eda879a56
Paul Oncoy <paul.oncoy@outlook.com>
2026-09-09 14:23:43 -0500

    Initial Commit

---
+++ LaSed/LaSedApp.swift
@@ -0,0 +1,17 @@
+//
+//  LaSedApp.swift
+//  LaSed
+//
+//  Created by Paul on 9/09/26.
+//
+
+import SwiftUI
+
+@main
+struct LaSedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ LaSedTests/LaSedTests.swift
@@ -0,0 +1,18 @@
+//
+//  LaSedTests.swift
+//  LaSedTests
+//
+//  Created by Paul on 9/09/26.
+//
+
+import Testing
+
+struct LaSedTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
